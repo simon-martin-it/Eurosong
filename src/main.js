8683d17ef9b3e338e778d892ca8c2519a99c4b5d
Simon Martin <simon.martin.it@outlook.com>
2021-11-15 10:55:40 +0100

    yeeeeeeeet

--- src/main.js
+++ src/main.js
@@ -3,14 +3,51 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
 
+/*
+//ES6
 let person = {
   firstname: "Simon",
   name: "Martin",
-  SayFullName(){
-    console.log('${this.firsntame"} ${this.name}');
+  Sayfullname: function(){
+    console.log(this.firstname + " " + this.name);
+  }
+};
+
+console.log(person.firstname);
+person.Sayfullname();
+
+let fullname = person.Sayfullname.bind(person);
+fullname();
+
+//ES5
+var person = {
+  firstname: "Simon",
+  name: "Martin",
+  sayFullName2s: function(){
+    var self = this;
+    setTimeout(function(){
+      console.log(self.firstname + " " + self.name);
+    }, 2000)
   }
 }
+*/
+
+person.sayFullName2s();
+
+//ES6
+var person = {
+  firstname: "Simon",
+  name: "Martin",
+  sayFullName2s() {
+    setTimeout(() => {
+      console.log(self.firstname + " " + self.name);
+    }, 2000);
+  }
+}
+
+person.sayFullName2s();
